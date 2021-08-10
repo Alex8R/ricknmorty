0b2b7ebc270f295892fc0612a51b220a43f04df6
Alex8R <repetzky.a@icloud.com>
2021-08-10 14:26:39 +0300

    added character search into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import './App.css'
+import { Preloader } from './components/Preloader/Preloader'
+import { Card } from './components/Card/Card'
+import { useDebounce } from './helpers/debounce'
+import { SearchBar } from './components/SeacrhBar/SearchBar'
+import { Container } from './components/Container/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CHARACTERS = gql`
+  query getList($searchText: String!) {
+    characters(filter: { name: $searchText }) {
+      info {
+        count
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        species
+        status
+        image
+      }
+    }
+  }
+`
+
+interface IData {
+  characters: ICharacters
 }
 
-export default App;
+interface ICharacters {
+  info: {
+    count: number
+    next: number
+    pages: number
+    prev: string | null
+  }
+  results: ICharacter[]
+}
+
+interface ICharacter {
+  id: string
+  image: string
+  name: string
+  species: string
+  status: string
+  __typename: string
+}
+
+type Options = {
+  searchText: string
+}
+
+function App() {
+  const [searchValue, setSearchValue] = useState<string>('')
+  const [searchText, setSearchText] = useState<string>('')
+
+  const debouncedSearchValue = useDebounce(searchValue)
+
+  const { loading, data } = useQuery<IData, Options>(CHARACTERS, {
+    variables: {
+      searchText,
+    },
+  })
+
+  const characters = useMemo(() => data?.characters?.results, [data])
+
+  useEffect(() => {
+    if (searchValue.length > 2) setSearchText(searchValue)
+  }, [debouncedSearchValue])
+
+  return (
+    <main className="App">
+      <SearchBar value={searchValue} setSearchValue={setSearchValue} />
+      {loading && <Preloader />}
+      <Container>
+        {characters && characters.length > 0 ?
+        characters.map(({ id, name, image, status, species }) => (
+          <Card
+            key={id}
+            species={species}
+            status={status}
+            name={name}
+            image={image}
+          />
+        )) : "Unknown character..."}
+      </Container>
+    </main>
+  )
+}
+
+export default App
